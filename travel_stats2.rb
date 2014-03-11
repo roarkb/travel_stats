7d570c95031fdbfa3824a3e6beb35014d2fb847c
roark <roarkb@gmail.com>
2014-03-11 10:11:44 +0800

    add debug statment

--- travel_stats2.rb
+++ travel_stats2.rb
@@ -2,17 +2,17 @@
 
 require "date"
 
-# "country name" => [list each date you entered the country]
+# "country name" => [list each date you entered a country]
 COUNTRIES = {
-  :thailand    => ["2013-3-12", "2013-12-23"],
+  :thailand    => ["2013-3-12", "2013-12-23", "2014-2-11", "2014-3-1"],
   :india       => ["2013-4-2", "2013-7-1"],
   :nepal       => ["2013-5-13"],
   :indonesia   => ["2013-7-11", "2013-10-5"],
   :malaysia    => ["2013-9-6", "2013-9-16"],
   :brunei      => ["2013-9-13"],
-  :singapore   => ["2013-11-4", "2013-12-13"],
+  :singapore   => ["2013-11-4", "2013-12-13", "2014-2-20"],
   :usa         => ["2013-11-10"],
-  :philippines => ["2013-11-25"],
+  :philippines => ["2013-11-25", "2014-3-8"],
   :myanmar     => ["2014-1-14"],
 }
 
@@ -49,6 +49,13 @@ visits.each do |visit|
 end
 
 puts
+
+# TODO: issue with using invert - if total days of a country is the same as another it will get overwritten because hash keys have to be unique
+puts "DEBUG:"
+p rollup
+p rollup.invert
+puts
+
 rollup.invert.sort.reverse.each do |days,country|
   printf("%-#{SPACER}s %s\n", "#{country}:", days)
 end
