f390492f0a2adce4cee9f9c5935f2a8d42483d8c
roark <roarkb@gmail.com>
2014-09-26 10:57:28 +0530

    better top places script

--- top_places.rb
+++ top_places.rb
@@ -1,22 +1,24 @@
 #!/usr/bin/env ruby
 
-# USAGE: ./top_places.rb <number of records>
+# USAGE: ./top_places.rb <min number of days and up>
 
 require 'nokogiri'
 require 'open-uri'
 
 URL = "http://roark.sleptlate.org/travel-stats/"
 RE = /^\d{1,3}\.\s/
-RECORDS = ARGV.first.to_i
+MIN_DAYS = ARGV.first.to_i
 
 page = Nokogiri::HTML(open(URL))
 text = page.at_css("#places").text.strip
 place_rollup = {}
-count = 0
 
-text.each do |line|
+#text.split("\n").each {|line| puts line}
+
+text.split("\n").each do |line|
   if line =~ RE
-    a = line.gsub(" \342\200\223 ", "|").gsub(RE, "").chomp.split("|")
+    #a = line.gsub(" \342\200\223 ", "|").gsub(RE, "").chomp.split("|")
+    a = line.gsub(/\s\W\s/, "|").gsub(RE, "").chomp.split("|")
     
     place = a[0]
     days = a[1].to_i
@@ -32,9 +34,7 @@ end
 puts
 
 place_rollup.sort_by {|k,v| v}.reverse.each do |place,days|
-  puts "#{place} - #{days}"
-  count += 1
-  break if count == RECORDS
+  puts "#{place} - #{days}" if days >= MIN_DAYS
 end
 
 puts
